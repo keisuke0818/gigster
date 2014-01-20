07798764b35a6bf9fcb1a74e4560a564d3938a33
keisuke kurata <kkurata@iacc.co.jp>
2014-01-20 12:30:47 +0900

    国修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,19 +8,19 @@ class ApplicationController < ActionController::Base
   def meta_defaults
     @meta_title = "Gigster"
     @meta_keywords = "Gig,Gigster,ギグ,ギグスター,ぎぐ,ぎぐすたー,ライブ,ライブ共有,音楽,コンサート"
-    @meta_description = "ギグ共有サービス"
+    @meta_description = "Share gig in the World/ギグ共有サービス"
     @meta_robots = "index,follow,noydir,noodp"
     @meta_viewport = "width=1000"
     @meta_format_detection = "telephone=no"
     @meta_X_UA_Compatible = "IE=edge"
 
     @meta_fg_appid = "153617311511977"
-    @meta_og_title = "Gigster/ギグ共有サービス"
+    @meta_og_title = "Gigster/Share gig in the World/ギグ共有サービス"
     @meta_og_type = "website"
     @meta_og_url = "http://gigster.jp/"
     @meta_og_image = "http://gigster.jp/assets/gigster.png"
     @meta_og_site_name = "Gigster"
-    @meta_og_description = "ギグ共有サービス"
+    @meta_og_description = "Share gig in the World/ギグ共有サービス"
     @meta_og_locale = "ja_JP"
 
   end
